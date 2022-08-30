33eb5809aef9339e22c2b6e4e11e3eacb5fd089c
maaghia <toubalmaria23@gmail.com>
2022-08-30 22:30:23 +0100

    final

--- hex.js
+++ hex.js
@@ -1,19 +1,28 @@
 let hexarr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f']
 let color = '#'
-let body = document.getElementsById(body);
+
 const getRandomNumberBetween = (min,max) => {return Math.floor(Math.random()*(max-min+1)+min);}
 //console.log(hexarr[getRandomNumberBetween(0,15)]) 
 
 const generateColor = () => {
+    color = '#'
     for (let i=0 ; i<6 ; i++){
         color = color + hexarr[getRandomNumberBetween(0,15)];
     }    
     return color;
 }
 
-let btn = document.getElementById("btn");
+let btn = document.querySelector("#btn");
+let bodyy = document.querySelector(".body");
+let colorCode = document.querySelector(".color");
 
 btn.addEventListener('click', event => {
-  body.style.backgroudcolor = generateColor();
+    generateColor();
+    bodyy.style.background = generateColor();
+   //console.log(bodyy.style.backgroudcolor)
+    colorCode.innerHTML = `${color}`
+   //console.log(color)
 });
+
+
 
